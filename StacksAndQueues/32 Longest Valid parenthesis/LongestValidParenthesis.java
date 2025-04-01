069b0b4011661c5d4777c260b1451c90121ae390
SNGSIY006 <SNGSIY006@myuct.ac.za>
2025-04-01 11:47:24 +0200

    Added Stack and Queues Folder and code implemetations.

---
+++ StacksAndQueues/32 Longest Valid parenthesis/LongestValidParenthesis.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+class Solution {
+    public int longestValidParentheses(String s) {
+        
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(-1);
+        int sLength = s.length();
+        int max_length=0;
+
+        for(int x = 0; x < sLength; x++){
+            char bracket = s.charAt(x);
+           
+            if(bracket == '('){
+                stack.push(x);
+            }else{
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(x);
+                    
+                }else{
+                    max_length= Math.max(max_length,x - stack.pop());
+                }
+                
+            }
+        } 
+
+
+        return max_length;
+    }
+}
